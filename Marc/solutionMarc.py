58d72601204c48af2744fe38d555512ee6f5e9de
marcwatine <marcwatineguiu@gmail.com>
2020-11-15 14:19:55 +0100

    hah

---
+++ Marc/solutionMarc.py
@@ -0,0 +1,214 @@
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
+from sklearn.gaussian_process import kernels
+
+domain = np.array([[0, 5]])
+kernel = Matern(length_scale=0.5)
+
+""" Solution """
+
+
+class BO_algo():
+    def __init__(self):
+        """Initializes the algorithm with a parameter configuration. """
+
+        # TODO: enter your code here
+        self.nu_v = 0.0001
+        self.mean_v = 1.5
+        self.nu_f = 0.15
+        self.K = 1.2
+        
+        
+        self.likelihood_f = gpytorch.likelihoods.GaussianLikelihood()
+        self.likelihood_v = gpytorch.likelihoods.GaussianLikelihood()
+        
+        self.kernel_f = gpytorch.kernels.ScaleKernel(base_kernel=gpytorch.kernels.MaternKernel(nu=2.5, lengthscale=torch.Tensor([0.5])), outputscale=0.5)
+
+        self.kernel_v = gpytorch.kernels.ScaleKernel(base_kernel=gpytorch.kernels.MaternKernel(nu=2.5, lengthscale=torch.Tensor([0.5])), outputscale=np.sqrt(2))
+       
+        self.gp_f = ExactGPModel(kernel = self.kernel_f, train_x = x, train_y = f, likelihood = self.likelihood_f)
+    
+        self.gp_v = ExactGPModel(mean=self.mean_v, kernel=self.kernel_v, train_x=x, train_y=v, likelihood=self.likelihood_v)
+        
+        
+        #self.f = matern...
+        #self.v = matern + mean...
+        #self.gp = gaussian process... :p
+        #ok c bon avec le ScaleKernel
+        
+        pass
+
+
+    def next_recommendation(self):
+        """
+        Recommend the next input to sample.
+
+        Returns
+        -------
+        recommendation: np.ndarray
+            1 x domain.shape[0] array containing the next point to evaluate
+        """
+
+        # TODO: enter your code here
+        # In implementing this function, you may use optimize_acquisition_function() defined below.
+        raise NotImplementedError
+
+
+    def optimize_acquisition_function(self):
+        """
+        Optimizes the acquisition function.
+
+        Returns
+        -------
+        x_opt: np.ndarray
+            1 x domain.shape[0] array containing the point that maximize the acquisition function.
+        """
+
+        def objective(x):
+            return -self.acquisition_function(x)
+
+        f_values = []
+        x_values = []
+
+        # Restarts the optimization 20 times and pick best solution
+        for _ in range(20):
+            x0 = domain[:, 0] + (domain[:, 1] - domain[:, 0]) * \
+                 np.random.rand(domain.shape[0])
+            result = fmin_l_bfgs_b(objective, x0=x0, bounds=domain,
+                                   approx_grad=True)
+            x_values.append(np.clip(result[0], *domain[0]))
+            f_values.append(-result[1])
+
+        ind = np.argmax(f_values)
+        return np.atleast_2d(x_values[ind])
+
+    def acquisition_function(self, x):
+        """
+        Compute the acquisition function.
+
+        Parameters
+        ----------
+        x: np.ndarray
+            x in domain of f
+
+        Returns
+        ------
+        af_value: float
+            Value of the acquisition function at x
+        """
+
+        # TODO: enter your code here
+        return acquisition_function_PI(self, x)
+        #return acquisition_function_EI(self, x)
+        #return acquisition_function_LCB(self, x)
+        
+        #raise NotImplementedError
+    def acquisition_function_PI(self, x):
+        """
+        Probability of Improvement acquisition function.
+
+        Parameters
+        ----------
+        x: np.ndarray
+            x in domain of f
+
+        Returns
+        ------
+        af_value: float
+            Value of the Probability of Improvement at x
+        """
+        
+        
+        
+
+    def add_data_point(self, x, f, v):
+        """
+        Add data points to the model.
+
+        Parameters
+        ----------
+        x: np.ndarray
+            Hyperparameters
+        f: np.ndarray
+            Model accuracy
+        v: np.ndarray
+            Model training speed
+        """
+
+        # TODO: enter your code here
+        
+   
+
+    def get_solution(self):
+        """
+        Return x_opt that is believed to be the maximizer of f.
+
+        Returns
+        -------
+        solution: np.ndarray
+            1 x domain.shape[0] array containing the optimal solution of the problem
+        """
+
+        # TODO: enter your code here
+        raise NotImplementedError
+
+
+""" Toy problem to check code works as expected """
+
+def check_in_domain(x):
+    """Validate input"""
+    x = np.atleast_2d(x)
+    return np.all(x >= domain[None, :, 0]) and np.all(x <= domain[None, :, 1])
+
+
+def f(x):
+    """Dummy objective"""
+    mid_point = domain[:, 0] + 0.5 * (domain[:, 1] - domain[:, 0])
+    return - np.linalg.norm(x - mid_point, 2)  # -(x - 2.5)^2
+
+
+def v(x):
+    """Dummy speed"""
+    return 2.0
+
+
+def main():
+    # Init problem
+    agent = BO_algo()
+
+    # Loop until budget is exhausted
+    for j in range(20):
+        # Get next recommendation
+        x = agent.next_recommendation()
+
+        # Check for valid shape
+        assert x.shape == (1, domain.shape[0]), \
+            f"The function next recommendation must return a numpy array of " \
+            f"shape (1, {domain.shape[0]})"
+
+        # Obtain objective and constraint observation
+        obj_val = f(x)
+        cost_val = v(x)
+        agent.add_data_point(x, obj_val, cost_val)
+
+    # Validate solution
+    solution = np.atleast_2d(agent.get_solution())
+    assert solution.shape == (1, domain.shape[0]), \
+        f"The function get solution must return a numpy array of shape (" \
+        f"1, {domain.shape[0]})"
+    assert check_in_domain(solution), \
+        f'The function get solution must return a point within the ' \
+        f'domain, {solution} returned instead'
+
+    # Compute regret
+    if v(solution) < 1.2:
+        regret = 1
+    else:
+        regret = (0 - f(solution))
+
+    print(f'Optimal value: 0\nProposed solution {solution}\nSolution value '
+          f'{f(solution)}\nRegret{regret}')
+
+
+if __name__ == "__main__":
+    main()
